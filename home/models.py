51af88b6040b60cb3280e3c9b4a36bda4da34e92
andruasha <anvolkov_1@edu.hse.ru>
2023-06-05 17:23:12 +0300

    update

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Summary(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+    path = models.CharField(max_length=256, blank=False)
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    context = {
+        'title': 'title'
+    }
+    return render(request, 'home/index.html', context)
 
 
 def home(request):
