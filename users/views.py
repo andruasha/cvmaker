d53393b7976d47109314465b4c11b4a8788dfd7f
andruasha <anvolkov_1@edu.hse.ru>
2023-06-06 10:36:35 +0300

    update

--- users/views.py
+++ users/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.models import User
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
