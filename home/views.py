19c21d0780a6df7208fd99415f8d6c4caa2c6cbe
andruasha <anvolkov_1@edu.hse.ru>
2023-06-07 15:58:22 +0300

    update

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from home.models import Summary
+from home.forms import SummaryForm
+from django.http import HttpResponse
+from io import BytesIO
+from reportlab.pdfgen import canvas
 
 
 def index(request):
@@ -11,4 +15,44 @@ def index(request):
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    if request.method == 'POST':
+        form = SummaryForm(data=request.POST)
+
+        name = request.POST['name']
+        surname = request.POST['surname']
+        email = request.POST['email']
+        number = request.POST['number']
+        education = request.POST['education']
+        experience = request.POST['experience']
+        skills = request.POST['skills']
+        languages = request.POST['languages']
+
+        buffer = BytesIO()
+        p = canvas.Canvas(buffer)
+
+        # Настройте документ PDF
+        p.setFont("Helvetica", 12)
+        p.drawString(100, 700, "Имя: {}".format(name))
+        p.drawString(100, 680, "Email: {}".format(email))
+        # ... добавьте другие данные резюме
+
+        # Завершите и сохраните документ PDF
+        p.showPage()
+        p.save()
+
+        # Получите содержимое PDF из буфера
+        buffer.seek(0)
+        pdf = buffer.getvalue()
+        buffer.close()
+
+        # Отправьте сгенерированный PDF в ответе HTTP
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="resume.pdf"'
+        response.write(pdf)
+
+        return response
+
+    else:
+        form = SummaryForm()
+    context = {'form': form}
+    return render(request, 'home/home.html', context)
